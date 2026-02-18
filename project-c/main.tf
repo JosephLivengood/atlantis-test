6aeb38b494a42164d3d3a1b622ca40aca0480fe4
Joseph Livengood <JoeyLivengood@gmail.com>
2026-02-17 22:43:38 -0800

    Add null_resource to all projects

--- project-c/main.tf
+++ project-c/main.tf
@@ -2,6 +2,8 @@ data "external" "slow_plan" {
   program = ["bash", "-c", "sleep 60 && echo '{}'"]
 }
 
+resource "null_resource" "noop" {}
+
 output "result" {
   value = data.external.slow_plan.result
 }
